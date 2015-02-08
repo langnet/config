a5ad70aaac791ee9d53ef6fac6c14631e8d80e51
root <root@VM_160_171_centos.(none)>
2015-02-08 19:30:19 +0800

    mysql login

--- mysql/login.sh
+++ mysql/login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/data/app/mysqlroot/base/bin/mysql -uroot -S /data/app/mysqlroot/data/3306/mysql.sock -p"pso@2015"
+/data/app/mysqlroot/base/bin/mysql -uroot -S /data/app/mysqlroot/data/3306/mysql.sock -p"111111"
